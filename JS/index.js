650b9bdf00c98d393fb089446a390f6168508c78
KvnKl007 <kkakkent99@gmail.com>
2024-04-04 16:29:47 +0200

    adding some JS

---
+++ JS/index.js
@@ -0,0 +1,39 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    var scrollPosition = window.scrollY;
+  
+    if (scrollPosition > 0) {
+      header.classList.add("fixed", "backshadow");
+    } else {
+      header.classList.remove("fixed", "backshadow");
+    }
+  });
+
+  const mainMenu = document.querySelector('.navbar', '.links', '.close');
+  const closeMenu = document.querySelector('.close');
+  const openMenu = document.querySelector('.hamburger');
+  const menu_items = document.querySelectorAll('.navbar li a');
+  
+  
+  
+  
+  openMenu.addEventListener('click',show);
+  closeMenu.addEventListener('click',close);
+  
+  // close menu when you click on a menu item 
+  menu_items.forEach(item => {
+      item.addEventListener('click',function(){
+          close();
+      })
+  })
+  
+  function show(){
+      mainMenu.style.display = 'block';
+      closeMenu.style.display = 'block';
+      openMenu.style.display = 'none';
+  }
+  function close(){
+      mainMenu.style.display = 'none';
+      openMenu.style.display = 'block';
+      closeMenu.style.display = 'none';
+    }
